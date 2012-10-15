a6d1c03f261f7bd7c4eb33ab4732537e1a68539c
Le Minh Duy <duylm@cogini.com>
2012-10-15 22:01:40 +0700

    config local

--- protected/config/modules.php
+++ protected/config/modules.php
@@ -5,9 +5,9 @@ return array(
        'gii'=>array(
              'class'=>'system.gii.GiiModule',
              'password'=>'1234',
-			 'generatorPaths'=>array(
-				'bootstrap.gii',
-        	 ),
+//			 'generatorPaths'=>array(
+//				'bootstrap.gii',
+//        	 ),
              'ipFilters'=>array('*.*.*.*')
           )
     )
